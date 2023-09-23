8d9d376ce8df906ecd9dc7f4362749aaa7dbe32a
Cnzw <git@cnzw.top>
2023-09-23 22:58:37 +0800

    feat: 定时消息提示

--- src/main/kotlin/cn/unimc/mcpl/uniauth/Task.kt
+++ src/main/kotlin/cn/unimc/mcpl/uniauth/Task.kt
@@ -2,6 +2,10 @@ package cn.unimc.mcpl.uniauth
 
 import org.bukkit.Bukkit
 import taboolib.common.platform.Schedule
+import taboolib.common.platform.function.console
+import taboolib.common.platform.function.onlinePlayers
+import taboolib.module.lang.asLangText
+import taboolib.module.lang.sendLang
 
 object Task {
 
@@ -9,8 +13,24 @@ object Task {
     fun taskLoginTimeout() {
         val tPlayer = AuthCache.getAuthTimeoutPlayers()
         tPlayer.forEach {
-            AuthCache.setStatus(it, AuthStatus.FAIL)
-            Bukkit.getPlayerExact(it)?.kickPlayer("") // TODO Lang
+            AuthCache.setStatus(it, AuthStatus.FAIL, Bukkit.getPlayerExact(it)!!.address!!)
+            Bukkit.getPlayerExact(it)?.kickPlayer(console().asLangText("kick-timeout"))
+        }
+    }
+
+    @Schedule(period = 100) // TODO
+    fun taskPrompt() {
+        for (player in onlinePlayers()) {
+            if (!AuthCache.isAuth(player.name)) {
+                if (AuthCache.getStatus(player.name) == AuthStatus.LOG) player.sendLang(
+                    "player-log-prompt",
+                    player.name
+                )
+                else if (AuthCache.getStatus(player.name) == AuthStatus.SCAN) player.sendLang(
+                    "player-scan-prompt",
+                    player.name
+                )
+            }
         }
     }
 }
