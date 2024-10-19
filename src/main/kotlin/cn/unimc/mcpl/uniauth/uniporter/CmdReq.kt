699633fbbdce33398a4a6f13573e513d19cf8ebe
Cnzw <git@cnzw.top>
2024-10-19 17:54:47 +0800

    CMD API 支持命令回显

--- src/main/kotlin/cn/unimc/mcpl/uniauth/uniporter/CmdReq.kt
+++ src/main/kotlin/cn/unimc/mcpl/uniauth/uniporter/CmdReq.kt
@@ -12,15 +12,20 @@ import io.netty.handler.codec.http.HttpMethod
 import io.netty.handler.codec.http.multipart.Attribute
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder
 import io.netty.handler.codec.http.multipart.InterfaceHttpData
+import org.bukkit.Bukkit
 import taboolib.common.platform.function.console
 import taboolib.common.platform.function.info
 import taboolib.common.util.sync
+import taboolib.module.lang.sendError
 import taboolib.module.lang.sendLang
+import taboolib.platform.util.bukkitPlugin
 import java.net.InetSocketAddress
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
 object CmdReq : UniporterHttpHandler {
     init {
-        if (UniAuth.config.getBoolean("login.cmd")) {
+        if (UniAuth.config.getBoolean("api.cmd")) {
             console().sendLang("console-api-cmd-enable")
         }
     }
@@ -29,7 +34,7 @@ object CmdReq : UniporterHttpHandler {
         val inSocket: InetSocketAddress = context?.channel()?.remoteAddress() as InetSocketAddress
         Utils.debugLog(inSocket.hostName + " - " + request?.method()?.name() + " " + path)
 
-        if (!UniAuth.config.getBoolean("login.cmd")) {
+        if (!UniAuth.config.getBoolean("api.cmd")) {
             context.writeAndFlush(Utils.build404Resp())?.addListener(ChannelFutureListener.CLOSE)
             return
         }
@@ -58,11 +63,26 @@ object CmdReq : UniporterHttpHandler {
             return
         }
         info("$path 执行指令 ${paramMap["cmd"].toString()}")
+
+        Bukkit.getScheduler().callSyncMethod(bukkitPlugin) {
+            console().performCommand(paramMap["cmd"].toString())
+        }
+        val completableFuture: CompletableFuture<List<String>> = CompletableFuture()
+
+        try {
+            completableFuture.get(5000, TimeUnit.MILLISECONDS)
+        } catch (e: Exception) {
+            console().sendError("console-api-cmd-err")
+            e.printStackTrace()
+        }
+        var result: List<String> = listOf()
+        completableFuture.complete(result)
+
         // 构建返回
         val optJson = mapOf(
             "code" to 200,
             "data" to mapOf(
-                "result" to sync { console().performCommand(paramMap["cmd"].toString()) }
+                "result" to result.joinToString("\n")
             )
         )
         // 输出返回
